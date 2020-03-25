6d7abe745ff4bf0f8e3f66f860328fbf6be2ef48
federfoschini <federfoschini@gmail.com>
2020-03-25 19:18:25 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,8 @@
 import psycopg2
 import os
 def connect():
-    db = {
-    "host":"localhost",
-    "database":"cah",
-    "user":"postgres",
-    "port":5433,
-    "password":"nopwd",
-
-    }
-    conn = psycopg2.connect(host = db['host'],database = db['database'],user = db['user'],port = db['port'],
-    password = db['password'],
+    
+    conn = psycopg2.connect(os.environ.connect('DATABASE_URL')
     )
 
     return conn
