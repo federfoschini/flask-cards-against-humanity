f03ef29518c0c57086c7ae7c61bede64244998f6
federfoschini <federfoschini@gmail.com>
2020-03-25 19:20:02 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,8 +2,7 @@ import psycopg2
 import os
 def connect():
     
-    conn = psycopg2.connect(os.environ.connect('DATABASE_URL')
-    )
+    conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
 
     return conn
 
